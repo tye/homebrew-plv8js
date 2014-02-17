6ac32ceb88ecf4d215933b88acdfe008d3af5b38
Tye Shavik <tyeshavik@gmail.com>
2014-02-17 18:26:36 -0500

    Use a specific commit as the head

--- plv8js.rb
+++ plv8js.rb
@@ -7,7 +7,7 @@ class Plv8js < Formula
   sha1 'abf7749b2f22cdc905d06183563491fe2ca621ec'
   version '1.4.1'
 
-  head 'https://code.google.com/p/plv8js.git'
+  head 'https://code.google.com/p/plv8js/tree/5e428ab67663015c71b2b6783d039e185cfcdaea'
 
   depends_on 'v8'
   depends_on 'postgresql'
