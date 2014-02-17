d1b78e4b95be1b802648d9c5e36274616dfb4542
Tye Shavik <tyeshavik@gmail.com>
2014-02-17 15:25:12 -0500

    Use edge version of v8

--- plv8js.rb
+++ plv8js.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Plv8js < Formula
   homepage 'https://code.google.com/p/plv8js'
 
-  url 'https://plv8js.googlecode.com/files/plv8-1.4.1.zip'
+  url 'https://plv8js.googlecode.com/archive/5e428ab67663015c71b2b6783d039e185cfcdaea.zip'
   sha1 'ceb7579b1fae1c1fe795c03a23471fdf9c7c469e'
   version '1.4.1'
 
