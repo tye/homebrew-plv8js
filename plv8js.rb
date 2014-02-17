0b96be1a6ab68022c872528145cc79d81e224157
Tye Shavik <tyeshavik@gmail.com>
2014-02-17 18:30:47 -0500

    Reverting some stuff

--- plv8js.rb
+++ plv8js.rb
@@ -3,11 +3,11 @@ require 'formula'
 class Plv8js < Formula
   homepage 'https://code.google.com/p/plv8js'
 
-  url 'https://plv8js.googlecode.com/archive/5e428ab67663015c71b2b6783d039e185cfcdaea.zip'
-  sha1 'abf7749b2f22cdc905d06183563491fe2ca621ec'
+  url 'https://plv8js.googlecode.com/files/plv8-1.4.1.zip'
+  sha1 'ceb7579b1fae1c1fe795c03a23471fdf9c7c469e'
   version '1.4.1'
 
-  head 'https://code.google.com/p/plv8js/tree/5e428ab67663015c71b2b6783d039e185cfcdaea'
+  head 'https://code.google.com/p/plv8js.git'
 
   depends_on 'v8'
   depends_on 'postgresql'
